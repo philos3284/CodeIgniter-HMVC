8f02280e086e89fa71a3a692642979517b47fc23
philos3284 <philosaphylas@gmail.com>
2017-02-13 20:23:30 -0800

    Update Welcome_controller.php
    Added a line to controller to fix callback error during form validation. Remember to add this line to every constructor if you plan on using custom functions in your validation. Inheriting from the main controller I created does not work apparently.

--- application/modules/welcome/controllers/Welcome_controller.php
+++ application/modules/welcome/controllers/Welcome_controller.php
@@ -7,8 +7,8 @@ class Welcome_controller extends MY_Controller
     public function __construct()
     {
       parent::__construct();
-      //Bug fix for CI form validation to function properly
-      //IMPORTANT: Add the line below to ALL controllers in order to use callbacks during form validation
+      //Bug fix for CI form validation to function properly.
+      //IMPORTANT: Add the line below to ALL controllers in order to use callbacks during form validation.
         $this->form_validation->CI =& $this;
     }
 	
