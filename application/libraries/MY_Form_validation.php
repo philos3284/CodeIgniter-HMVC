f0e72ca94f46f1ab437c5ad4a8bbc7ae178e78df
philos3284 <philosaphylas@gmail.com>
2017-02-13 19:37:46 -0800

    Update MY_Form_validation.php
    Bug fix for callback error during form validation

--- application/libraries/MY_Form_validation.php
+++ application/libraries/MY_Form_validation.php
@@ -1,15 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class MY_Form_validation extends CI_Form_validation {
-
+class MY_Form_validation extends CI_Form_validation 
+{
   public $CI; // Propery set to fix callback error
   
-	/**
-	 * Extends CI Form Validation.
-	 *
-	 * Fix for (HMVC) Extension.
-	 */
+/**
+ * Extends CI Form Validation.
+ *
+ * Fix for (HMVC) Extension.
+ */
 	 
   public function run($module = '', $group = '')
   {
@@ -17,19 +17,3 @@ class MY_Form_validation extends CI_Form_validation {
     return parent::run($group);
   }
 }
-
-  
-/*
- 
-Original Code:
--------------------
-
-  class MY_Form_validation extends CI_Form_validation {
-
-    function run($module = '', $group = ''){
-      (is_object($module)) AND $this->CI = &$module;
-      return parent::run($group);
-    }
-  }
-  
-*/
