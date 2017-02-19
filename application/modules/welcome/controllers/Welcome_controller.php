d73c30479aab054a8eab220a09d7c51200a81c6d
philos3284 <philosaphylas@gmail.com>
2017-02-18 19:40:32 -0800

    Update Welcome_controller.php

--- application/modules/welcome/controllers/Welcome_controller.php
+++ application/modules/welcome/controllers/Welcome_controller.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome_controller extends MY_Controller 
+class Welcome_controller extends MX_Controller 
 {
  
     public function __construct()
