8cbfafc84639e4e7c1c7ae497c8afa5d4d693c9e
Curt <philosaphylas@gmail.com>
2017-03-02 23:48:05 -0800

    Update Welcome_controller.php

--- application/modules/welcome/controllers/Welcome_controller.php
+++ application/modules/welcome/controllers/Welcome_controller.php
@@ -7,8 +7,8 @@ class Welcome_controller extends MX_Controller
     public function __construct()
     {
       parent::__construct();
-      //Bug fix for CI form validation to function properly.
-      //IMPORTANT: Add the line below to ALL controllers in order to use callbacks during form validation.
+     // Bug fix for CI form validation to function properly.
+     // IMPORTANT: Add this line to all controllers to prevent callback errors when using form validation.
         $this->form_validation->CI =& $this;
     }
 	
