a387d7d595cc2db7f08292d678f770fe60f43b1a
Curt <philosaphylas@gmail.com>
2017-03-02 23:43:24 -0800

    Update Rename.php

--- application/modules/NEW_module/controllers/Rename.php
+++ application/modules/NEW_module/controllers/Rename.php
@@ -7,7 +7,7 @@ class Rename extends MX_Controller
     public function __construct()
     {
       parent::__construct();
-
+      //IMPORTANT: Add the line below to ALL controllers in order to use callbacks during form validation.
         $this->form_validation->CI =& $this;
     }
 	
