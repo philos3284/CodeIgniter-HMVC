c45317b9f21b3f9249a4db0dcb3af4cd3786a4f3
Curt <philosaphylas@gmail.com>
2017-03-08 20:32:01 -0800

    Update Rename_model.php

--- application/modules/NEW_module/models/Rename_model.php
+++ application/modules/NEW_module/models/Rename_model.php
@@ -9,14 +9,14 @@ class Rename_model extends CI_Model
     }
 
 
-  public function get_table()
-	{
-	  $table = "";
-    return $table;
-	}
+    public function get_table()
+    {
+      $table = "INSERT_TABLE_NAME_HERE";
+        return $table;
+    }
 
     public function get($order_by) 
-	{
+    {
       $table = $this->get_table();
       $this->db->order_by($order_by);
       $query=$this->db->get($table);
@@ -24,7 +24,7 @@ class Rename_model extends CI_Model
     }
 
     public function get_with_limit($limit, $offset, $order_by) 
-	{
+    {
       $table = $this->get_table();
       $this->db->limit($limit, $offset);
       $this->db->order_by($order_by);
@@ -33,7 +33,7 @@ class Rename_model extends CI_Model
     }
 
     public function get_where($id) 
-	{
+    {
       $table = $this->get_table();
       $this->db->where('id', $id);
       $query=$this->db->get($table);
@@ -41,34 +41,34 @@ class Rename_model extends CI_Model
     }
 
     public function get_where_custom($col, $value) 
-	{
+    {
       $table = $this->get_table();
       $this->db->where($col, $value);
       $query=$this->db->get($table);
       return $query;
     }
     public function _insert($data) 
-	{
+    {
       $table = $this->get_table();
       $this->db->insert($table, $data);
     }
 
     public function _update($id, $data) 
-	{
+    {
       $table = $this->get_table();
       $this->db->where('id', $id);
       $this->db->update($table, $data);
     }
 
     public function _delete($id) 
-	{
+    {
       $table = $this->get_table();
       $this->db->where('id', $id);
       $this->db->delete($table);
     }
 
     public function count_where($column, $value) 
-	{
+    {
       $table = $this->get_table();
       $this->db->where($column, $value);
       $query=$this->db->get($table);
@@ -77,7 +77,7 @@ class Rename_model extends CI_Model
     }
 
     public function count_all() 
-	{
+    {
       $table = $this->get_table();
       $query=$this->db->get($table);
       $num_rows = $query->num_rows();
@@ -85,7 +85,7 @@ class Rename_model extends CI_Model
     }
 
     public function get_max() 
-	{
+    {
       $table = $this->get_table();
       $this->db->select_max('id');
       $query = $this->db->get($table);
@@ -95,7 +95,7 @@ class Rename_model extends CI_Model
     }
 
     public function _custom_query($mysql_query) 
-	{
+    {
       $query = $this->db->query($mysql_query);
       return $query;
     }
