2655850aedaed14ed878561d549e165260ad251b
Curt <philosaphylas@gmail.com>
2017-03-17 12:16:14 -0700

    Update Rename.php

--- application/modules/NEW_module/controllers/Rename.php
+++ application/modules/NEW_module/controllers/Rename.php
@@ -3,7 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Rename extends MX_Controller 
 {
- 
+    private $model = $this->load->model('INSERT_MODEL_NAME_HERE');   
+
     public function __construct()
     {
       parent::__construct();
@@ -11,7 +12,7 @@ class Rename extends MX_Controller
      // NOTE: If you autoload the form_validation class you don't need to call it here
         $this->load->library('form_validation');
         $this->form_validation->CI =& $this;
-        $this->load->model('INSERT_MODEL_NAME_HERE');
+        $this->model;
     }
 	
     public function index()
@@ -21,58 +22,58 @@ class Rename extends MX_Controller
 	
     public function get($order_by) 
     {
-      $query = $this->INSERT_MODEL_NAME_HERE->get($order_by);
+      $query = $this->model->get($order_by);
       return $query;
     }
 
     public function get_with_limit($limit, $offset, $order_by) 
     {
-      $query = $this->INSERT_MODEL_NAME_HERE->get_with_limit($limit, $offset, $order_by);
+      $query = $this->model->get_with_limit($limit, $offset, $order_by);
       return $query;
     }
 
     public function get_where($id) 
     {
-      $query = $this->INSERT_MODEL_NAME_HERE->get_where($id);
+      $query = $this->model->get_where($id);
       return $query;
     }
 
     public function get_where_custom($col, $value) 
     {
-      $query = $this->INSERT_MODEL_NAME_HERE->get_where_custom($col, $value);
+      $query = $this->model->get_where_custom($col, $value);
       return $query;
     }
 
     private function insert($data) 
     {
-      $this->INSERT_MODEL_NAME_HERE->insert($data);
+      $this->model->insert($data);
     }
 
     private function update($id, $data) 
     {
-      $this->INSERT_MODEL_NAME_HERE->update($id, $data);
+      $this->model->update($id, $data);
     }
 
     private function delete($id) 
     {
-      $this->INSERT_MODEL_NAME_HERE->delete($id);
+      $this->model->delete($id);
     }
 
     public function count_where($column, $value) 
     {
-      $count = $this->INSERT_MODEL_NAME_HERE->count_where($column, $value);
+      $count = $this->model->count_where($column, $value);
       return $count;
     }
 
     public function get_max() 
     {
-      $max_id = $this->INSERT_MODEL_NAME_HERE->get_max();
+      $max_id = $this->model->get_max();
       return $max_id;
     }
 
     private function custom_query($mysql_query) 
     {
-      $query = $this->INSERT_MODEL_NAME_HERE->custom_query($mysql_query);
+      $query = $this->model->custom_query($mysql_query);
       return $query;
     }
 
