2818cc60647fb808ccfa1400a39823ebc0d767be
Curt <philosaphylas@gmail.com>
2017-03-17 11:58:09 -0700

    Update Rename_model.php

--- application/modules/NEW_module/models/Rename_model.php
+++ application/modules/NEW_module/models/Rename_model.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+private $table = 'INSERT_TABLE_NAME_HERE';
+
 class Rename_model extends CI_Model 
 {
     public function __construct()
@@ -8,78 +10,62 @@ class Rename_model extends CI_Model
       parent::__construct();
     }
 
-
-    public function get_table()
-    {
-      $table = "INSERT_TABLE_NAME_HERE";
-        return $table;
-    }
-
     public function get($order_by) 
     {
-      $table = $this->get_table();
       $this->db->order_by($order_by);
-      $query=$this->db->get($table);
+      $query=$this->db->get($this->table);
       return $query;
     }
 
     public function get_with_limit($limit, $offset, $order_by) 
     {
-      $table = $this->get_table();
       $this->db->limit($limit, $offset);
       $this->db->order_by($order_by);
-      $query=$this->db->get($table);
+      $query=$this->db->get($this->table);
       return $query;
     }
 
     public function get_where($id) 
     {
-      $table = $this->get_table();
       $this->db->where('id', $id);
-      $query=$this->db->get($table);
+      $query=$this->db->get($this->table);
       return $query;
     }
 
     public function get_where_custom($col, $value) 
     {
-      $table = $this->get_table();
       $this->db->where($col, $value);
-      $query=$this->db->get($table);
+      $query=$this->db->get($this->table);
       return $query;
     }
-    public function _insert($data) 
+    protected function insert($data) 
     {
-      $table = $this->get_table();
-      $this->db->insert($table, $data);
+      $this->db->insert($this->table, $data);
     }
 
-    public function _update($id, $data) 
+    protected function update($id, $data) 
     {
-      $table = $this->get_table();
       $this->db->where('id', $id);
-      $this->db->update($table, $data);
+      $this->db->update($this->table, $data);
     }
 
-    public function _delete($id) 
+    protected function delete($id) 
     {
-      $table = $this->get_table();
       $this->db->where('id', $id);
-      $this->db->delete($table);
+      $this->db->delete($this->table);
     }
 
     public function count_where($column, $value) 
     {
-      $table = $this->get_table();
       $this->db->where($column, $value);
-      $query=$this->db->get($table);
+      $query=$this->db->get($this->table);
       $num_rows = $query->num_rows();
       return $num_rows;
     }
 
     public function count_all() 
     {
-      $table = $this->get_table();
-      $query=$this->db->get($table);
+      $query=$this->db->get($this->table);
       $num_rows = $query->num_rows();
       return $num_rows;
     }
@@ -88,13 +74,13 @@ class Rename_model extends CI_Model
     {
       $table = $this->get_table();
       $this->db->select_max('id');
-      $query = $this->db->get($table);
+      $query = $this->db->get($this->table);
       $row=$query->row();
       $id=$row->id;
       return $id;
     }
 
-    public function _custom_query($mysql_query) 
+    protected function custom_query($mysql_query) 
     {
       $query = $this->db->query($mysql_query);
       return $query;
