ee27519379740315cebe1a351a619afc61847c17
Curt <philosaphylas@gmail.com>
2017-03-17 12:26:23 -0700

    Update Welcome_model.php

--- application/modules/welcome/models/Welcome_model.php
+++ application/modules/welcome/models/Welcome_model.php
@@ -2,9 +2,19 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome_model extends CI_Model 
 {
+    private $table = 'INSERT_TABLE_NAME_HERE';
+    
     public function __construct()
     {
       parent::__construct();
+    }
+
+    // Example:
+    public function rename($param) 
+    {
+      $this->db->order_by($param);
+      $query=$this->db->get($this->table);
+      return $query;
     }
     
 }
