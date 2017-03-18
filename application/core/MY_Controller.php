8e6753a631839c938e6c3afd416e33ef0069a7b0
Curt <philosaphylas@gmail.com>
2017-03-18 12:00:15 -0700

    Create MY_Controller.php

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Controller extends MX_Controller 
+{
+    protected $model;
+
+    public function __construct() 
+    {
+      parent::__construct();
+    }
+    
+}
