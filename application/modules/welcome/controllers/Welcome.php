77a155fb129087f26c8312c53ab87c490531b0cc
Curt <philosaphylas@gmail.com>
2017-03-18 12:04:02 -0700

    Update Welcome.php

--- application/modules/welcome/controllers/Welcome.php
+++ application/modules/welcome/controllers/Welcome.php
@@ -1,9 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Welcome extends MX_Controller 
+class Welcome extends MY_Controller 
 {
-    private $model = 'INSERT_MODEL_NAME_HERE';
-
     public function __construct()
     {
       parent::__construct();
@@ -11,7 +9,8 @@ class Welcome extends MX_Controller
      // NOTE: If you autoload the form_validation class you don't need to call it here
         $this->load->library('form_validation');
         $this->form_validation->CI =& $this; // Bug fix for CI form validation to function properly.
-        $this->load->model($this->model);
+        $this->load->model('mdl_Welcome');
+        $this->model = $this->mdl_Welcome;
     }
 	
     public function index()
@@ -22,8 +21,7 @@ class Welcome extends MX_Controller
     // Example:
     public function rename($param) 
     {
-      $model = $this->model;
-      $query = $this->$model->rename($param);
+      $query = $this->model->rename($param);
       return $query;
     }
 	
