757d8c95402df1b26562de39de531817c7941cca
Curt <philosaphylas@gmail.com>
2017-03-19 11:31:59 -0700

    Update Welcome.php

--- application/modules/welcome/controllers/Welcome.php
+++ application/modules/welcome/controllers/Welcome.php
@@ -5,16 +5,12 @@ class Welcome extends MY_Controller
 {
     public function __construct()
     {
-      parent::__construct();
-    /* 
-        NOTE: Add these lines to all controllers to prevent callback errors when using form validation. If you autoload the form_validation class you don't need to call it here.
-    */
-
+        parent::__construct();
+     // NOTE: Add these lines to all controllers to prevent callback errors when using form validation. If you autoload the form_validation class you don't need to call it here.
      // Load library
         $this->load->library('form_validation');
-     // Fix for CI form_validation to function properly.
+     // Fix for CI form_validation to function properly
         $this->form_validation->CI =& $this;
-	    
      // Load model
         $this->load->model('mdl_Welcome');
         $this->model = $this->mdl_Welcome;
