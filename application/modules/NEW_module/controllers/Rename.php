a151e53f7e8352fa91df411c871af766e037ad48
Curt <philosaphylas@gmail.com>
2017-03-19 11:29:09 -0700

    Update Rename.php

--- application/modules/NEW_module/controllers/Rename.php
+++ application/modules/NEW_module/controllers/Rename.php
@@ -5,16 +5,12 @@ class Rename extends MY_Controller
 {
     public function __construct()
     {
-      parent::__construct();
-    /* 
-        NOTE: Add these lines to all controllers to prevent callback errors when using form validation. If you autoload the form_validation class you don't need to call it here.
-    */
-
+        parent::__construct();
+     // NOTE: Add these lines to all controllers to prevent callback errors when using form validation. If you autoload the form_validation class you don't need to call it here.
      // Load library
         $this->load->library('form_validation');
      // Fix for CI form_validation to function properly
         $this->form_validation->CI =& $this;
-	    
      // Load model
         $this->load->model('INSERT_MODEL_HERE');
         $this->model = $this->INSERT_MODEL_HERE;
